bb3b2af4a0c875a161baacf816b4409b5804da39
Essen, Rick van <rick.vanessen@wur.nl>
2022-06-09 15:27:40 +0200

    Add scripts to help on the event

---
+++ scripts/load_files.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+IMAGE_FILE=""
+SIMULATION_FILES=""
+TEAM_NAME=""
+
+while getopts "h?i:s:n:" opt; do
+  case "$opt" in
+    h|\?)
+      printf "Script to load docker images and simulation files from teams in the Field Robot Event.\nOptions: \n\n-i IMAGE_NAME\n-s SIMULATION_FILES\n-n TEAM_NAME\n"
+      exit 0
+      ;;
+    i)  IMAGE_FILE=$OPTARG
+      ;;
+    s)  SIMULATION_FILES=$OPTARG
+      ;;
+    n)  TEAM_NAME=$OPTARG
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] && shift
+
+if [[ -z ${IMAGE_FILE} || -z "${SIMULATION_FILES}" || -z "${TEAM_NAME}" ]]
+then 
+    printf "Script to load docker images and simulation files from teams in the Field Robot Event.\nOptions: \n\n-i IMAGE_NAME\n-s SIMULATION_FILES\n-n TEAM_NAME\n"
+    exit 0    
+fi
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+TEAM_FILES_DIR=$(readlink -f "${SCRIPT_DIR}/../competition_files/${TEAM_NAME}")
+
+if [ -d ${TEAM_FILES_DIR} ] 
+then
+    echo "Directory ${TEAM_FILES_DIR} already exists. Clearing folder now..." 
+    rm -r "${TEAM_FILES_DIR}"
+    
+    if [ $? -ne 0 ] ; then
+        echo "Failed to clear content of dir ${TEAM_FILES_DIR}!"
+        exit 1
+    fi
+fi
+
+mkdir -p "${TEAM_FILES_DIR}"
+if [ $? -ne 0 ] ; then
+    echo "Failed to create dir ${TEAM_FILES_DIR}!"
+    exit 1
+fi
+
+echo "Unzipping simulation files..."
+unzip -qq ${SIMULATION_FILES} -d ${TEAM_FILES_DIR}
+
+# Put files folder higher if placed in simulation_files subfolder
+if [ -d "${TEAM_FILES_DIR}/simulation_files" ] 
+then
+    pushd ${TEAM_FILES_DIR}
+        mv simulation_files/* .
+        rm -r simulation_files
+        
+        if [ $? -ne 0 ] ; then
+            echo "Failed to move dir ${TEAM_FILES_DIR}/simulation_files!"
+            exit 1
+        fi
+    popd 
+fi
+
+# Remove unneeded folders
+for dir in ${TEAM_FILES_DIR}/*; do
+    [ "$dir" = "${TEAM_FILES_DIR}/robot_packages" ] && continue
+    [ "$dir" = "${TEAM_FILES_DIR}/rviz" ] && continue
+    rm -rf "$dir"
+
+    if [ $? -ne 0 ] ; then
+        echo "Failed to move dir ${TEAM_FILES_DIR}/${dir}!"
+        exit 1
+    fi
+done
+
+# Source: https://gist.github.com/stefanvangastel/d6b5b38e8716ea102b651c67c100225f
+echo "Loading Docker image..."
+LOAD_RESULT=$(docker load -i ${IMAGE_FILE})
+LOAD_IMAGE_NAME=${LOAD_RESULT#*: }
+
+echo "Retagging Docker image from ${LOAD_IMAGE_NAME} to ${TEAM_NAME}/robot_workspace"
+docker tag ${LOAD_IMAGE_NAME} "${TEAM_NAME}/robot_workspace"
+
+echo "All done."
---
+++ scripts/prepare_run.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+TEAM_NAME=""
+
+while getopts "h?n:" opt; do
+  case "$opt" in
+    h|\?)
+      printf "Script to prepare docker images and simulation files from teams in the Field Robot Event.\nOptions:\n\n-n TEAM_NAME\n"
+      exit 0
+      ;;
+    n)  TEAM_NAME=$OPTARG
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] && shift
+
+if [[ -z "${TEAM_NAME}" ]]
+then 
+    printf "Script to prepare docker images and simulation files from teams in the Field Robot Event.\nOptions:\n\n-n TEAM_NAME\n"
+    exit 0    
+fi
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+SIMULATION_FILES_DIR=$(readlink -f "${SCRIPT_DIR}/../simulation_files")
+TEAM_FILES_DIR=$(readlink -f "${SCRIPT_DIR}/../competition_files/${TEAM_NAME}")
+
+if [ ! -d "${TEAM_FILES_DIR}" ] 
+then
+    echo "Could not load files of ${TEAM_NAME}. Are the files loaded?"
+    exit 1
+fi
+
+echo "Copying simulation files..."
+for folder in robot_packages rviz
+do
+    rm -rf "${SIMULATION_FILES_DIR}/${folder}"
+     
+    if [ $? -ne 0 ] ; then
+        echo "Failed to remove dir ${SIMULATION_FILES_DIR}/${folder}!"
+        exit 1
+    fi
+
+    cp -r "${TEAM_FILES_DIR}/${folder}" ${SIMULATION_FILES_DIR}
+
+    if [ $? -ne 0 ] ; then
+        echo "Failed to copy dir ${TEAM_FILES_DIR}/${folder}!"
+        exit 1
+    fi
+done
+
+echo "Retagging Docker image from ${TEAM_NAME}/robot_workspace to robot_workspace" 
+docker tag "${TEAM_NAME}/robot_workspace" "robot_workspace"
+
+if [ $? -ne 0 ] ; then
+    echo "Could not retag Docker image from ${TEAM_NAME}/robot_workspace to robot_workspace! Does the Docker image exists?" 
+    exit 1
+fi
+
+echo "All done."
